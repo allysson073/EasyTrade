1bb38f62d9c9abbb911f7feae6d745ff419a0187
allysson073 <allysson073@gmail.com>
2023-08-19 17:27:04 -0300

    criei a tela de login

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=devide-width, initial-scale=1.0">
+        <title>EasyTrade</title>
+    </head>
+    <body>
+        <h1> EasyTrade </h1>
+        <form>
+            <label>Login:</login>
+            <input type="text" required>
+            <label>Login:</login>
+            <input type="text" required>
+            <input type="submit" value="Entrar">
+        </form>
+    </body>
+</html>
